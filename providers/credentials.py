53046bae1a92df8aaafdc481f29a1d0e656dcca8
Daniel Bruno <dbruno@fedoraproject.org>
2014-10-25 14:52:09 -0600

    Credentials class

---
+++ providers/credentials.py
@@ -0,0 +1,16 @@
+
+class identify(object):
+
+    def __init__(self, provider):
+        self.provider = provider
+        if self.provider == 'aws':
+            self.credentials = {"key": "", "secret": ""}
+        elif self.provider == "digitalocean":
+            self.credentials = {"key": "", "secret": ""}
+
+    def getKey(self):
+        return self.credentials['key']
+
+    def getSecret(self):
+        return self.credentials['secret']
+
